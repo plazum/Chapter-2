541b6077376ce88120969efdbe3dfff986e3663a
plazum <34277374+plazum@users.noreply.github.com>
2020-06-10 16:24:50 +0800

    Homework 2

--- app/src/main/java/chapter/android/aweme/ss/com/chapter2/view/RelativeLayoutActivity.java
+++ app/src/main/java/chapter/android/aweme/ss/com/chapter2/view/RelativeLayoutActivity.java
@@ -3,6 +3,8 @@ package chapter.android.aweme.ss.com.chapter2.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import chapter.android.aweme.ss.com.chapter2.R;
 
@@ -11,5 +13,9 @@ public class RelativeLayoutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relativelayout);
+
+        TextView tv_center = findViewById(R.id.tv_center);
+        int n = ((RelativeLayout) tv_center.getParent()).getChildCount();
+        tv_center.setText(String.format("%d", n));
     }
 }
